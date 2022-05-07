bdf1e51a6a5d727b6a77ff7e8566e7de67120f9e
Ivar Scholten <ivar.scholten@protonmail.com>
2022-05-07 18:32:14 +0200

    split data fetching logic into their own functions

--- src/savefile.cpp
+++ src/savefile.cpp
@@ -5,11 +5,30 @@
 
 namespace savepatcher {
 
+std::string SaveFile::name() {
+    auto section = saveHeader();
+    return getCharRange(0, magic.NAME_OFFSET, section);
+}
+
+bool SaveFile::active() {
+    return getByteRange(magic.ACTIVE_INDEX)[0];
+}
+
+unsigned long long SaveFile::steamId() {
+    auto section = getByteRange(magic.STEAM_ID_INDEX, magic.STEAM_ID_INDEX + magic.STEAM_ID_LENGTH);
+    return toLittleEndian(section);
+}
+
+std::vector<uint8_t> SaveFile::saveHeader() {
+    return getByteRange(magic.SAVE_HEADER_INDEX, magic.SAVE_HEADER_INDEX + magic.SAVE_HEADER_LENGTH + 1);
+}
+
 std::vector<uint8_t> SaveFile::loadFile(const std::string& filename) {
     std::ifstream file(filename, std::ios::binary);
 
     if (!file.is_open()) {
-        throw std::runtime_error("Could not open file");
+        auto errorMsg = fmt::format("Could not open file '{}'", filename);
+        throw std::runtime_error(errorMsg);
     };
 
     // Read the file into a byte array
@@ -20,36 +39,56 @@ std::vector<uint8_t> SaveFile::loadFile(const std::string& filename) {
     file.close();
 
     // Validate the file is an Elden Ring save by checking if it starts with ascii "BND"
-    for (int i = 0; i < 3; i++) {
-        std::bitset<8> byte(data[i]);
-        char ascii = char(byte.to_ulong());
-
-        switch (ascii) {
-            case 'B':
-                break;
-            case 'N':
-                break;
-            case 'D':
-                break;
-            default:
-                auto errorMsg = fmt::format("File '{}' is not a valid Elden Ring save file.", filename);
-                throw std::runtime_error(errorMsg);
-        }
-    };
+    if (getCharRange(0, 3) != "BND") {
+        auto errorMsg = fmt::format("File '{}' is not a valid Elden Ring save file.", filename);
+        throw std::runtime_error(errorMsg);
+    }
 
     return data;
 }
 
-std::vector<uint8_t> SaveFile::get(int beginOffset, int endOffset, std::vector<uint8_t>& data) {
-    if (beginOffset < 0 || endOffset > data.size() || beginOffset > endOffset) {
-        throw std::runtime_error("Invalid offset range");
+std::vector<uint8_t> SaveFile::getByteRange(int beginOffset, int endOffset, std::vector<uint8_t>& section) {
+    if (beginOffset < 0 || endOffset > section.size() || beginOffset > endOffset) {
+        auto errorMsg = fmt::format("Invalid offset range: [{}, {}]", beginOffset, endOffset);
+        throw std::runtime_error(errorMsg);
     }
 
-    return std::vector<uint8_t>(data.begin() + beginOffset, data.begin() + endOffset);
+    return std::vector<uint8_t>(section.begin() + beginOffset, section.begin() + endOffset);
 }
 
-void SaveFile::print(int beginOffset, int endOffset, std::vector<uint8_t>& data) {
-    std::vector<uint8_t> dataToPrint = get(beginOffset, endOffset, data);
+std::string SaveFile::getCharRange(int beginOffset, int endOffset, std::vector<uint8_t>& section) {
+    std::string chars;
+
+    for (int i = beginOffset; i < endOffset; i++) {
+        if (section[i] == 0)
+            continue;
+
+        std::bitset<8> byte(section[i]);
+        char ascii = char(byte.to_ulong());
+        chars += ascii;
+    }
+
+    return chars;
+}
+
+unsigned long long SaveFile::toLittleEndian(int beginOffset, int endOffset, std::vector<uint8_t>& section) {
+    std::string output;
+
+    // Convert the bytes to little endian by reversing their order
+    std::vector<uint8_t> reversed = section;
+    std::reverse(reversed.begin(), reversed.end());
+
+    for (int i = beginOffset; i < endOffset; i++) {
+        std::bitset<16> byte(reversed[i]);
+        output += fmt::format("{:02x}", byte.to_ullong());
+    };
+
+    // Convert output to a 16 bit unsigned long long
+    return std::stoul(output, nullptr, 16);
+};
+
+void SaveFile::print(int beginOffset, int endOffset, std::vector<uint8_t>& section) {
+    std::vector<uint8_t> dataToPrint = getByteRange(beginOffset, endOffset, section);
     int index = 0;
 
     for (auto& i : dataToPrint) {
@@ -66,46 +105,5 @@ void SaveFile::print(int beginOffset, int endOffset, std::vector<uint8_t>& data)
 
     fmt::print("\n");
 }
-
-unsigned long long SaveFile::steamId() {
-    auto steamIdSection = get(magic.STEAM_ID_INDEX, magic.STEAM_ID_INDEX + magic.STEAM_ID_LENGTH);
-    std::string steamId;
-
-    // Stored in little endian so we need to reverse the order of the bytes
-    std::reverse(steamIdSection.begin(), steamIdSection.end());
-
-    for (int i = 0; i < magic.STEAM_ID_LENGTH; i++) {
-        std::bitset<16> byte(steamIdSection[i]);
-
-        steamId += fmt::format("{:02x}", byte.to_ullong());
-    }
-
-    // Convert to 16 bit integer
-    return std::stoul(steamId, nullptr, 16);
-};
-
-std::string SaveFile::name() {
-    std::string name;
-
-    for (int i = 0; i < magic.CHAR_NAME_LENGTH; i++) {
-        if (saveHeader()[i] == 0)
-            continue;
-
-        std::bitset<8> byte(saveHeader()[i]);
-        char ascii = char(byte.to_ulong());
-
-        name += ascii;
-    }
-
-    return name;
-}
-
-bool SaveFile::active() {
-    return get(magic.CHAR_ACTIVE_INDEX)[0];
-}
-
-std::vector<uint8_t> SaveFile::saveHeader() {
-    return get(magic.SAVE_HEADER_INDEX, magic.SAVE_HEADER_INDEX + magic.SAVE_HEADER_LENGTH + 1);
-};
 
 }; // namespace savepatcher
--- src/savefile.h
+++ src/savefile.h
@@ -10,27 +10,35 @@ class SaveFile {
     std::vector<uint8_t> data;
 
     // Get a range of bytes from the save file
-    std::vector<uint8_t> get(int beginOffset, int endOffset, std::vector<uint8_t>& data);
-    std::vector<uint8_t> get(int beginOffset, int endOffset) { return get(beginOffset, endOffset, data); }
-    std::vector<uint8_t> get(std::vector<uint8_t> data) { return get(0, data.size(), data); }
-    std::vector<uint8_t> get(int offset) { return get(offset, offset + 1, data); }
+    std::vector<uint8_t> getByteRange(int beginOffset, int endOffset, std::vector<uint8_t>& section);
+    std::vector<uint8_t> getByteRange(int beginOffset, int endOffset) { return getByteRange(beginOffset, endOffset, data); }
+    std::vector<uint8_t> getByteRange(std::vector<uint8_t> data) { return getByteRange(0, data.size(), data); }
+    std::vector<uint8_t> getByteRange(int offset) { return getByteRange(offset, offset + 1, data); }
 
-    // The raw bytes from the save header
+    // Get a range of bytes from the save file as ascii
+    std::string getCharRange(int beginOffset, int endOffset, std::vector<uint8_t>& section);
+    std::string getCharRange(int beginOffset, int endOffset) { return getCharRange(beginOffset, endOffset, data); }
+
+    // Convert a value to a little endian 16 bit integer
+    unsigned long long toLittleEndian(int beginOffset, int endOffset, std::vector<uint8_t>& section);
+    unsigned long long toLittleEndian(std::vector<uint8_t>& section) { return toLittleEndian(0, section.size(), section); }
+
+    // The raw bytes pointing to the save header
     std::vector<uint8_t> saveHeader();
 
-    // copied from https://github.com/BenGrn/EldenRingSaveCopier/blob/v0.0.2-alpha/EldenRingSaveCopy/Saves/Model/SaveGame.cs#L11
+    // Copied from https://github.com/BenGrn/EldenRingSaveCopier/blob/v0.0.2-alpha/EldenRingSaveCopy/Saves/Model/SaveGame.cs#L11
     struct magic {
         int SAVE_HEADER_INDEX = 0x1901D0E;
         int SAVE_HEADER_LENGTH = 0x24C;
-        int CHAR_NAME_LENGTH = 0x22;
-        int CHAR_ACTIVE_INDEX = 0x1901D04;
-        int CHAR_LEVEL_OFFSET = 0x22;
         int STEAM_ID_INDEX = 0x19003B4;
         int STEAM_ID_LENGTH = 0x8;
-        //const static int SLOT_START_INDEX = 0x310;
-        //const static int SLOT_LENGTH = 0x280000;
-        //const static int SAVE_HEADERS_SECTION_START_INDEX = 0x19003B0;
-        //const static int SAVE_HEADERS_SECTION_LENGTH = 0x60000;
+        int ACTIVE_INDEX = 0x1901D04;
+        int NAME_OFFSET = 0x22;
+        int LEVEL_OFFSET = 0x22;
+        //int SLOT_START_INDEX = 0x310;
+        //int SLOT_LENGTH = 0x280000;
+        //int SAVE_HEADERS_SECTION_START_INDEX = 0x19003B0;
+        //int SAVE_HEADERS_SECTION_LENGTH = 0x60000;
     } magic;
 
   public:
@@ -41,7 +49,7 @@ class SaveFile {
     // Get the name of the character in save slot 0
     std::string name();
 
-    // Get the steam ID currently used by the save file.
+    // Get the steam ID currently used by the save file
     unsigned long long steamId();
 
     // Check wether save slot 0 is active
